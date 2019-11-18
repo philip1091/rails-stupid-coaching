d0306e87eb84821479330d7ee0f160c7067d6bec
Jean Philippe Hicintuka <jeanphilh@outlook.com>
2019-11-18 13:41:22 +0800

    stupid coach rails done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get 'pages/ask_coach'
+  get '/ask', to: 'questions#ask', as: :ask
+
+  # get 'pages/answer_coach'
+  get '/answer', to: 'questions#answer', as: :answer
 end
